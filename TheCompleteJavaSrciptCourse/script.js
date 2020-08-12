857fd079a997c77f7aa31445f0ec10aeedf3ec0d
agrisl <40560038+agrisl@users.noreply.github.com>
2020-08-12 14:19:28 +0300

    Delete script.js

--- TheCompleteJavaSrciptCourse/script.js
+++
@@ -1,9 +0,0 @@
-var output=document.getElementById("output");
-var q=prompt("c");
-var x=1;
-while(x<q){
-    output.innerHTML+="Value of x= "+x+"<br>";
-    x++;
-}
-
-output.innerHTML+="Final value of x= "+x+"<br>";
